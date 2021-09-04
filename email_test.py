6ab0974805a6f56df85249ad2f46af8f1a81613d
blasto <55928203+blastomussa@users.noreply.github.com>
2021-09-03 21:22:41 -0400

    Create email_test.py
    Requires Google API Credentials: https://developers.google.com/gmail/api/quickstart/python?hl=en

---
+++ email_test.py
@@ -0,0 +1,88 @@
+from __future__ import print_function
+import base64
+import os.path
+from email.mime.text import MIMEText
+from httplib2.error import ServerNotFoundError
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from email.mime.multipart import MIMEMultipart
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://mail.google.com/']
+
+def create_message(sender, to, subject, message_text):
+    """Create a message for an email.
+
+    Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+
+    Returns:
+    An object containing a base64url encoded email object.
+    """
+    emailMsg = message_text
+    mimeMessage = MIMEMultipart()
+    mimeMessage['to'] = 'jcourtney@gcvs.org'
+    mimeMessage['subject'] = subject
+    mimeMessage.attach(MIMEText(emailMsg, 'plain'))
+    raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
+    return raw_string
+
+
+def send_message(service, user_id, raw_string):
+    """Send an email message.
+
+    Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
+
+    Returns:
+    Sent Message.
+    """
+    try:
+        message = (service.users().messages().send(userId=user_id, body={'raw': raw_string}).execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except(ServerNotFoundError,HttpError) as error:
+        print('An error occurred: %s' % error)
+
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    # Call the Gmail API
+    raw_string = create_message("sender@gmail.com","recipient@gmail.com","TEST","TEST")
+    print(send_message(service,"me",raw_string))
+
+
+if __name__ == '__main__':
+    main()
